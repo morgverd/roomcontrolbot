32be20ee8a8fa198c32514d47877c316c225d438
MorgVerd <morgverd@gmail.com>
2020-04-14 00:33:40 +0100

    Remove test script

--- test.sh
+++
@@ -1,8 +0,0 @@
-r=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
-read -p "To confirm type '$r' >> " check
-if test "$check" = "$r"; then
-	echo "ay"
-else
-	echo "Incorrect Confirmation Code"
-	exit
-fi
